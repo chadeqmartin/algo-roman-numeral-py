00511b1dac744c2829b77edce90238162835377b
chadeqmartin <chadeqmartin@gmail.com>
2023-10-05 15:01:33 -0400

    updating with working solution to challenge

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,24 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    romanAnswer = ''
+
+    romanValues = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'LC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    }
+    for key in romanValues:
+        while num >= romanValues[key]:
+            romanAnswer += key
+            num -= romanValues[key]
+    
+    return romanAnswer
